b6743c87495d13980e32d4e4d2d0255386ffd4bc
joseedivandro <jose.edivandro.ja@outlook.com>
2023-02-10 18:06:25 -0300

    corrigindo bugs e mudando nome e foto do perfil

--- src/components/Usuario.js
+++ src/components/Usuario.js
@@ -1,11 +1,37 @@
+import { useState } from "react"
+
 export default function Usuario(){
+
+let fotoInicial = "assets/catanacomics.svg"
+const nomeInicial = "catanacomics"
+const [foto, setFoto] = useState(fotoInicial)
+const [nomeDoUsuario, setNomeDoUsuario] = useState(nomeInicial)
+
+
+function trocarFoto(){
+  const fotoNova = prompt("qual foto deseja colocar?");
+  if(fotoNova.length !=0 || fotoNova != undefined || fotoNova != null){
+    setFoto(fotoNova);
+  }
+}
+
+function trocarNome(){
+  const novoNome = prompt("qual nome deseja colocar?");
+  if(novoNome.length !=0 || novoNome != undefined || novoNome != null){
+    setNomeDoUsuario(novoNome);
+  }
+}
+
+
+
+
    return (
     <div class="usuario">
-    <img src="assets/catanacomics.svg" alt="imagem de perfil"/>
+    <img data-test="profile-image"src={foto} alt={foto} onClick={trocarFoto}/>
     <div class="texto">
       <span>
-        <strong>catanacomics</strong>
-        <ion-icon name="pencil"></ion-icon>
+        <strong>{nomeDoUsuario}</strong>
+        <ion-icon data-test= "edit-name" name="pencil"onClick={trocarNome} ></ion-icon>
       </span>
     </div>
   </div>
